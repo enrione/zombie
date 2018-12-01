9e9c5a3c0386f318fd7007bf1ab4120ec7048c8e
Italo Enrione <italo.enrione@gmail.com>
2018-12-01 12:32:42 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :zombies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to:'zoombies#index'
+  root to:'zombies#index'
 end
